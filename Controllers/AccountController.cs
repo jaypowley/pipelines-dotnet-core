79d9a2179400d15f8739b4c6980c9b79796c910d
Jason Powley <jason@jasonpowley.com>
2021-06-11 09:11:11 -0400

    Testing Pipeline

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pipelines_dotnet_core.Models;
+
+namespace pipelines_dotnet_core.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
